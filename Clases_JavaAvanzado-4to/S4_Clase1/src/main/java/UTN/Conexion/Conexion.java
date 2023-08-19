f9b26bc12d802cc6bf8153be630ad790db7be5ed
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-08-19 17:09:17 -0300

    Clase 1 de java
    Agrego la clase 1 de java

---
+++ Clases_JavaAvanzado-4to/S4_Clase1/src/main/java/UTN/Conexion/Conexion.java
@@ -0,0 +1,24 @@
+package UTN.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection getConnection(){
+        Connection conexion = null;
+
+        var baseDatos = "estudiantes";
+        var url = "jdbc:mysql://localhost:3306/"+baseDatos;
+        var usuario = "root";
+        var password = "admin";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario,password);
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("Ocurrio en error en la conexion de tipo: "+ e.getMessage());
+        }
+        return conexion;
+
+    }
+}
