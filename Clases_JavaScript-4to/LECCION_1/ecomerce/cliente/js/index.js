30c93198b2095542aaa2e8886b02295cfb78e8e5
LucasSalinas <111472464+LucasSalinas@users.noreply.github.com>
2023-09-06 20:05:58 -0700

    Update index.js

--- Clases_JavaScript-4to/LECCION_1/ecomerce/cliente/js/index.js
+++ Clases_JavaScript-4to/LECCION_1/ecomerce/cliente/js/index.js
@@ -18,13 +18,22 @@ products.forEach((product) => {
     content.append(buybutton);
 
     buybutton.addEventListener("click", () => {
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart);
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            cart.map((prod)=> {
+                if(prod.id === product.id){
+                    prod.quanty ++;
+                }
+            });
+        }else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            })
+            console.log(cart);
+        }
         })
 });
