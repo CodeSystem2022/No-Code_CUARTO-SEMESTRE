7b0ee93d965bf434a574aae25f0e4b6f205a9448
Andres Winckler <andreswinckler@gmail.com>
2023-09-14 08:11:35 -0300

    java avanzado

--- Clases_JavaAvanzado-4to/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Clases_JavaAvanzado-4to/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,33 @@
 package utn.estudiantes;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.line.separator();
 
 	public static void main(String[] args) {
-		SpringApplication.run(EstudiantesApplication.class, args);
-	}
 
+		logger.info("Iniciando la aplicación...");
+		//Levantar fabrica de Spring
+		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Aplicación Finalizada!");
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl+"Ejecutando el meétodo run de Spring..."+nl);
+	}
 }
