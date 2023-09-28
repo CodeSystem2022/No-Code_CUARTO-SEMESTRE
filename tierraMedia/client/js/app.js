c3f649f3d33c00f0f69e6126e0d750d39bf05e18
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-09-27 23:15:45 -0300

    arreglo el codigo y lo likeo correctamente

--- tierraMedia/client/js/app.js
+++ tierraMedia/client/js/app.js
@@ -1,9 +1,10 @@
 const contenedorMain = document.querySelector(".productos"); 
+const tituloEncabezado = document.querySelector(".titulo");
 
 
 productos.forEach((producto) =>{
     const divContenedorPorducto1 = document.createElement("div");
-
+    tituloEncabezado.textContent = "Remeras"
     divContenedorPorducto1.innerHTML = `
         <div class="contenedor-producto">
             <div class="div-imagen-producto">
@@ -11,7 +12,7 @@ productos.forEach((producto) =>{
             </div>
             <div class="div-nombre-precio">
                  <h2 class="div-nombre">${producto.nombreProducto}</h2>
-                 <p class="div-precio">${producto.precio}</p>
+                 <p class="div-precio">$${producto.precio}</p>
                 <buttom class="btn-agregar-carrito" id="categoria-remeras-producto-1">Agregar al Carrito</buttom>
             </div>
         </div>
