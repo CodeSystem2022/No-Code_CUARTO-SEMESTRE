b169bc31a7b2dbf733a7a0f7e8372861e7464dad
Franco Ariel <103206342+Chorifrann@users.noreply.github.com>
2023-10-05 18:40:38 -0300

    Delete Clases_JavaScript-4to/PERN-stack directory

--- Clases_JavaScript-4to/PERN-stack/src/controllers/auth.controller.js
+++
@@ -1,7 +0,0 @@
-export const singin = (req,res) => res.send("ingresando");
-
-export const singup = (req,res) => res.send("registrando");
-
-export const singout = (req,res) => res.send("cerrando sesion");
-
-export const profile = (req,res) => res.send("perfil de usuario");
--- Clases_JavaScript-4to/PERN-stack/src/index.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(3000);
-
-console.log("Server on port", 3000)
